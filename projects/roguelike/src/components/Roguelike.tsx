377f7f429539449a92682bd916f538d8cae75fd8
Zack Ward <zack@zadiastudio.com>
2016-08-28 10:35:13 -0600

    Initial commit

---
+++ projects/roguelike/src/components/Roguelike.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+export interface RoguelikeProps {
+
+}
+
+export interface RoguelikeState {
+
+}
+
+export class Roguelike extends React.Component<RoguelikeProps, RoguelikeState> {
+
+    render() {
+        return (
+            <div className="roguelike">
+                <p>Roguelike</p>
+            </div>
+        );
+    }
+}
---
+++ projects/roguelike/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import {Roguelike} from "./components/Roguelike";
+
+ReactDOM.render(
+    <Roguelike />,
+    document.getElementById('app')
+);
